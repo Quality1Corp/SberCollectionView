e16ef1c9eb48739cc13262e76033ece362c8a739
Mikhail Mishurenkov <m.mishurenkov@yandex.ru>
2023-05-03 17:07:19 +0300

    Initial Commit

---
+++ SberCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SberCollectionView
+//
+//  Created by MikhaiL on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
